3f0bb50478b846040aae64208b356585b3e2234b
Lyo Nya Zhee-Zhin <lazhizhin@yandex.ru>
2023-12-26 10:44:02 +0100

    Day 12 part 2

--- src/Day12.kt
+++ src/Day12.kt
@@ -3,28 +3,50 @@ fun main() {
         return mapRow.trim('.').split(".").filter { it.isNotEmpty() }.map { it.length.toLong() }.toList() == damagedComp
     }
 
-    fun getAllCombinations(mapRow: String): List<String> {
-        var stringsToCheck = mutableListOf(mapRow)
-        for (i in 1..mapRow.count { it == '?' }) {
-            val stringsToAdd = mutableListOf<String>()
-            for (s in stringsToCheck) {
-                stringsToAdd.add(s.replaceFirst("?", "."))
-                stringsToAdd.add(s.replaceFirst("?", "#"))
-            }
-            stringsToCheck = stringsToAdd
+    fun checkValidSubpattern(mapRow: String, damagedComp: List<Long>): Boolean {
+        val subpattern = mapRow.trim('.').split(".").filter { it.isNotEmpty() }.map { it.length.toLong() }.toList()
+        if (subpattern.size > damagedComp.size) {
+            return false
         }
-        return stringsToCheck.filter { !it.contains("?") }.map { it.replace("""[.]+""".toRegex(), ".") }
+        return subpattern.dropLast(1) == damagedComp.slice(0 until subpattern.size - 1)
+    }
+
+    fun getAllCombinations(mapRow: String, damagedComp: List<Long>): Map<String, Long> {
+        val stringsToCheck = mutableMapOf(mapRow to 1L)
+        while (stringsToCheck.keys.any { it.contains("?") }) {
+            val originalString = stringsToCheck.keys.first { it.contains("?") }
+            val originalCount = stringsToCheck.remove(originalString)
+            for (newChar in listOf(".", "#")) {
+                val newString = originalString.replaceFirst("?", newChar).replace("[.]+".toRegex(), ".")
+                if (checkValidSubpattern(newString.split('?').first(), damagedComp)) {
+                    stringsToCheck[newString] = stringsToCheck.getOrDefault(newString, 0L) + originalCount!!
+                }
+            }
+        }
+        return stringsToCheck
     }
 
     fun part1(input: List<String>): Long {
         return input.sumOf { row ->
             val (mapRow, damagedComp) = row.split(" ")
-            getAllCombinations(mapRow).count {
-                checkValid(it, damagedComp.split(",").map { it.toLong() })
-            }.toLong()
+            val damagedCompLong = damagedComp.split(",").map { it.toLong() }
+            getAllCombinations(mapRow, damagedCompLong).filter { combination ->
+                checkValid(combination.key, damagedCompLong)
+            }.map { it.value }.sum()
+        }
+    }
+
+    fun part2(input: List<String>): Long {
+        return input.sumOf { row ->
+            val (mapRow, damagedComp) = row.split(" ")
+            val damagedCompLong = ("$damagedComp,").repeat(5).dropLast(1).split(",").map { it.toLong() }
+            getAllCombinations("$mapRow?".repeat(5).dropLast(1), damagedCompLong).filter { combination ->
+                checkValid(combination.key, damagedCompLong)
+            }.map { it.value }.sum()
         }
     }
 
     val input = readInput("Day12")
     part1(input).println()
+    part2(input).println()
 }
